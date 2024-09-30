265768dd58d6da03358704fd490a8c7e110a0260
Rafa130206 <rafabompadre@gmail.com>
2024-09-30 16:01:45 -0300

    Imagem civilações antigas.

--- cp5/src/app/civilizacoes/page.tsx
+++ cp5/src/app/civilizacoes/page.tsx
@@ -1,4 +1,4 @@
-/*import civilizacoes from "./assets/venus.jpg"*/
+import Imagem from "next/image"
 
 export default function Civilizacoes() {
     return (
@@ -6,7 +6,7 @@ export default function Civilizacoes() {
             <p>
                 Velikovsky argumenta que esses cataclismos astronômicos foram observados e registrados por civilizações antigas em mitos, textos religiosos e relatos históricos. Ele sugere que histórias como as pragas do Egito, a travessia do Mar Vermelho, e as batalhas descritas no Livro de Josué na Bíblia foram, na verdade, descrições de eventos causados por perturbações cósmicas. O "sol parado" no céu, por exemplo, seria uma referência a uma alteração no movimento da Terra devido a uma interação com outro corpo celeste.
             </p>
-            <img src={civilizacoes} alt="Civilizações Antigas" />
+            <Imagem src="/assets/civilizacao.jpg" alt="Vênus" width={629} height={473} />
         </div>
     )
 }
