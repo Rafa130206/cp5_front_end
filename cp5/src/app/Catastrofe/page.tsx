6fc0ba6ae0a6411fbb4e48af3c0d8c270fb07bf3
Rafa130206 <rafabompadre@gmail.com>
2024-09-30 16:10:47 -0300

    Imagem catástrofe adicionada.

--- cp5/src/app/Catastrofe/page.tsx
+++ cp5/src/app/Catastrofe/page.tsx
@@ -1,4 +1,4 @@
-/*import catastrofe from "./assets/venus.jpg"*/
+import Imagem from 'next/image'
 
 export default function catastrofe() {
     return (
@@ -6,7 +6,7 @@ export default function catastrofe() {
             <p>
                 Velikovsky também propôs que essas interações planetárias não foram únicas, mas ocorreram várias vezes ao longo da história. Após a passagem de Vênus, ele sugere que Marte também passou perto da Terra em cerca de 700 a.C., causando outros desastres naturais e eventos que foram registrados nas mitologias e tradições de culturas antigas.
             </p>
-            <img src={catastrofe} alt="Catástrofe" />
+            <Imagem src="/assets/catastrofe.jpg" alt='Catástrofe' width={1120} height={630}/>
         </div>
     )
 }
