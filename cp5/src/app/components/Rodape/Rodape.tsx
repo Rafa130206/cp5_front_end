a909e573449500c1e26dab7c4be79f5e6b66203c
Rafa130206 <rafabompadre@gmail.com>
2024-09-30 11:08:15 -0300

    Rodapé criado.

---
+++ cp5/src/app/components/Rodape/Rodape.tsx
@@ -0,0 +1,8 @@
+export default function Rodape() {
+    return (
+        <footer>
+            <h2>CP5</h2>
+            <p>Copyright © 2024 - Todos os direitos reservados</p>
+        </footer>
+    )
+}
